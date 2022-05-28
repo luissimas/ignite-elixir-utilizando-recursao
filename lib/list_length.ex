6591ec4b07614153080f0aa3faae2955e8d3694d
Luís Simas <luissimas@protonmail.com>
2022-05-28 15:11:38 -0300

    feat: list length

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,8 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
-  end
+  @spec call(list) :: integer
+  def call(list), do: length(list, 0)
+
+  @spec length(list, integer) :: integer
+  def length([], acc), do: acc
+  def length([_ | t], acc), do: length(t, acc + 1)
 end
